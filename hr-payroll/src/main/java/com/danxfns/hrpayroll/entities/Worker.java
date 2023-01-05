189016b04987a7cc3a34ec603c9dd66d32a94dad
Danielle <daniso_4@hotmail.com>
2023-01-05 19:56:56 -0300

    Rest Template

---
+++ hr-payroll/src/main/java/com/danxfns/hrpayroll/entities/Worker.java
@@ -0,0 +1,16 @@
+package com.danxfns.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Worker implements Serializable {
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+}
